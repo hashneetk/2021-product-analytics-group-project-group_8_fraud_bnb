c2e8d78ac3367f287b67ad5a425fb7bdd70584ce
audreybarszcz <abarszcz@dons.usfca.edu>
2021-06-04 21:04:19 -0700

    more text on home page

--- code/app/routes.py
+++ code/app/routes.py
@@ -2,6 +2,8 @@ from app import application
 from flask import render_template
 
 @application.route('/')
-@application.route('/index')
+@application.route('/about')
 def index():
-    return('<h1> Welcome to FraudBnB </h1>')
+    """Index page: Renders about.html with team member names and project description"""
+#     return('<h1> Welcome to FraudBnB </h1>')
+    return (render_template('about.html', authors='Hashneet Kaur, Phillip Navo, Shruti Roy, Vaishnavi Kashyap, Sandhya Kiran, Kaiqi Guo, Jordan Uyeki, and Audrey Barszcz', description='*project description goes here*'))
