60ab26fe00abab04b276243b289a1deddf6dd171
audreybarszcz <abarszcz@dons.usfca.edu>
2021-06-04 20:41:44 -0700

    updated home page

--- code/app/routes.py
+++ code/app/routes.py
@@ -4,4 +4,4 @@ from flask import render_template
 @application.route('/')
 @application.route('/index')
 def index():
-    return('<h1> Hello world </h1>')
+    return('<h1> Welcome to FraudBnB </h1>')
