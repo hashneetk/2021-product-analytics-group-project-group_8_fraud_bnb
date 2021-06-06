5e64e4d262484cebd3bcabe0ad986ae5b07a2058
Shruti Roy <tarunayroy@Shruti-Roy.local>
2021-06-05 18:46:22 -0700

    adding aws related

---
+++ eb_deploy.sh
@@ -0,0 +1,2 @@
+eb init group-8-fraudbnb-app -p Docker -r us-west-2
+eb create group-8-fraudbnb-env --envvar AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvar AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
