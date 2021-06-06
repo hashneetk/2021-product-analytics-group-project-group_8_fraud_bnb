bf0e364e953d1474afd844e8f46b95cbfd3dcd22
lemon <72583756+kguo8@users.noreply.github.com>
2021-06-06 00:25:10 -0700

    create selen_scrap.py and add html templates

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 import os
 
 application = Flask(__name__)
+application.secret_key = os.urandom(24)
 
 from app import routes
--- code/app/routes.py
+++ code/app/routes.py
@@ -1,5 +1,15 @@
 from app import application
 from flask import render_template
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from wtforms import SubmitField, TextField, validators
+
+from selen_scrap import get_info
+
+
+class URLFile(FlaskForm):
+    url = TextField('ListURL', [validators.required(), validators.URL()])
+    submit = SubmitField('Submit')
 
 
 @application.route('/')
@@ -12,3 +22,29 @@ def index():
     Phillip Navo, Shruti Roy, Vaishnavi Kashyap, Sandhya Kiran, \
     Kaiqi Guo, Jordan Uyeki, and Audrey Barszcz',
             description='*project description goes here*'))
+
+
+@application.route('/search', methods=['POST', 'GET'])
+def search():
+    url = URLFile()
+    if url.validate_on_submit():
+        page_info = get_info(url.url.data)
+
+        return render_template(
+            'search_result.html', authors='Hashneet Kaur, \
+            Phillip Navo, Shruti Roy, Vaishnavi Kashyap, Sandhya Kiran, \
+            Kaiqi Guo, Jordan Uyeki, and Audrey Barszcz',
+                description='*:)*',
+                list_name=page_info['listing_name'],
+                host_name=page_info['host_name'],
+                reviews=page_info['reviews'],
+                rating=page_info['rating']
+        )
+
+    return render_template(
+            'search.html', authors='Hashneet Kaur, \
+        Phillip Navo, Shruti Roy, Vaishnavi Kashyap, Sandhya Kiran, \
+        Kaiqi Guo, Jordan Uyeki, and Audrey Barszcz',
+                description='*:)*',
+                form=url,
+    )
