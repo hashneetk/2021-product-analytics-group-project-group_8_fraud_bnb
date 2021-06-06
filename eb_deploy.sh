52989eef4063aa7dfe80ca37b141d74513f3a189
Sandhya Kiran <sandhyakiran04@gmail.com>
2021-06-06 15:27:24 -0700

    add key

--- eb_deploy.sh
+++ eb_deploy.sh
@@ -1,2 +1,2 @@
-eb init group-8-fraudbnbb -p Docker -r us-west-2
-eb create group-8-fraudbnbb-env --envvar AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvar AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+eb init group-8-fraudbnb --key $PEM_NAME -p Docker -r us-west-2
+eb create group-8-fraudbnb-env --envvar AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvar AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
