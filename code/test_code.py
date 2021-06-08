fbf30e5bcab708066225d89e289bb63689804fa3
pnavo <philnavo@gmail.com>
2021-06-07 20:03:44 -0700

    inital pytest file

---
+++ code/test_code.py
@@ -0,0 +1,12 @@
+import selenium
+
+def test_submit_button():
+	'''Testing to verifiy that the submit button works appropriately
+	and redirects to the correct page.'''
+	pass
+
+
+
+
+if __name__ == "__main__":
+	test_submit_button()
