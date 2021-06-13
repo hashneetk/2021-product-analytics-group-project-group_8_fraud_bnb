498680ca88b3dc13616e3b64accf700b7ef974c9
lemon <72583756+kguo8@users.noreply.github.com>
2021-06-13 16:25:05 -0700

    update db password

--- code/config.py
+++ code/config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = \
-        'postgresql://postgres:msds603-8@fraudbnb-8-1.cysu6ff5rfrq.us-west-2.rds.amazonaws.com:5432/postgres'
+        f'postgresql://postgres:{os.environ["DB_PASSWORD"]}@fraudbnb-8-1.cysu6ff5rfrq.us-west-2.rds.amazonaws.com:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
