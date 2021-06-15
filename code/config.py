1ed141a16d951112fa5f60b5d32ecd8733ce4387
pnavo <philnavo@gmail.com>
2021-06-14 21:22:34 -0700

    pep8

--- code/config.py
+++ code/config.py
@@ -5,5 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = \
-        f'postgresql://postgres:{os.environ["DB_PASSWORD"]}@fraudbnb-8-1.cysu6ff5rfrq.us-west-2.rds.amazonaws.com:5432/postgres'
+        f'postgresql://postgres:{os.environ["DB_PASSWORD"]}\
+        @fraudbnb-8-1.cysu6ff5rfrq.us-west-2.rds.amazonaws.com:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
